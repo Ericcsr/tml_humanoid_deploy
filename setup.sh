ea928ce2fe3e8538cee7cf2e5ef3bb05756e0d69
ericcsr <ericcsr-bm@u5c8c6888886d59.ant.amazon.com>
2026-01-08 21:33:59 -0500

    add setup scripts

---
+++ setup.sh
@@ -0,0 +1,8 @@
+conda create -n bm python=3.10.18
+conda activate bm
+pip install mujoco joblib onnxruntime pybullet scipy torch PyYAML opencv-python
+cd ..
+git clone https://github.com/unitreerobotics/unitree_sdk2_python.git
+cd unitree_sdk2_python
+pip install -e . --no-deps
+cd ../tml_humanoid_deploy
