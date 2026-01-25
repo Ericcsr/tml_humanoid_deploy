a8b8b2f4a048b17331053043815f5377eaafe9a4
ericcsr <ericcsr@u65f247c5ce545c.ant.amazon.com>
2026-01-25 11:50:55 -0800

    update setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 conda create -n bm python=3.10.18
 conda activate bm
-pip install mujoco joblib onnxruntime pybullet scipy torch PyYAML opencv-python
+pip install mujoco joblib onnxruntime pybullet scipy torch PyYAML opencv-python redis
+sudo apt install redis-server
 cd ..
 git clone https://github.com/unitreerobotics/unitree_sdk2_python.git
 cd unitree_sdk2_python
